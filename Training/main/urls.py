62470274791ff5e0efee7969fc5b8365f0002e79
Nawaf Alahmadi <nawaf.m.alahmadi@gmail.com>
2025-02-27 13:55:15 +0300

    Home page with HTML and CSS

--- Training/main/urls.py
+++ Training/main/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
+  path("", views.home_view, name= "index"),
   path("html/introduction/", views.introduction, name= "introduction"),
   path("css/introduction/", views.css_introduction, name="css_introduction")
 ]
--- Training/main/views.py
+++ Training/main/views.py
@@ -3,11 +3,14 @@ from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 
-# Introduction
+# Introduction HTML
 def introduction(request:HttpRequest):
-
   return render(request, "main/introduction.html")
 
-
+# Introduction CSS
 def css_introduction(request:HttpRequest):
   return render(request,"main/css_introduction.html")
+
+# Build 
+def home_view(request:HttpRequest):
+  return render(request, "main/index.html")
