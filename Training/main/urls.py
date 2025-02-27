71881889152e0a5af482a97025e1762d85095a4e
Nawaf Alahmadi <nawaf.m.alahmadi@gmail.com>
2025-02-27 10:35:57 +0300

    Css Part

--- Training/main/urls.py
+++ Training/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-  path("html/introduction/", views.introduction, name= "introduction")
+  path("html/introduction/", views.introduction, name= "introduction"),
+  path("css/introduction/", views.css_introduction, name="css_introduction")
 ]
--- Training/main/views.py
+++ Training/main/views.py
@@ -5,4 +5,9 @@ from django.http import HttpRequest, HttpResponse
 
 # Introduction
 def introduction(request:HttpRequest):
+
   return render(request, "main/introduction.html")
+
+
+def css_introduction(request:HttpRequest):
+  return render(request,"main/css_introduction.html")
