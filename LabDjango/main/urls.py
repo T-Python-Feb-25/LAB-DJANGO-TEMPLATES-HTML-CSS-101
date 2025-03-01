fecf3de86d2c2cd1f3e9919a4526a9484ad0d7b4
abdulrahman aidarous <abdulrahmanaid.21@gmail.com>
2025-03-01 16:41:49 +0300

    Solution for LAB-DJANGO-TEMPLATES-HTML-CSS-101

---
+++ LabDjango/main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+app_name="main"
+urlpatterns=[
+    path("html/introduction/",views.favourite_team_view,name="favourite_team"),
+    path("learn/",views.learn_view,name="learn_view"),
+    path("learn/form/",views.form_practice_view,name="form_practice_view"),
+    path("learn/ccs/",views.ccs_practice_view,name="ccs_practice_view"),
+    path("css/introduction/",views.css_introduction_view,name="css_introduction.view"),
+    path("article/ai/",views.ai_revolution_view,name="ai_revolution_view"),
+    path("careers/cv/",views.careers_cv_view,name="careers_cv_view")
+
+]
---
+++ LabDjango/main/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpRequest
+
+def favourite_team_view(requset:HttpRequest):
+    return render(requset,"main/FavouriteTeam.html")
+def learn_view(requset:HttpRequest):
+    return render(requset,"main/Learn.html")
+def form_practice_view(request:HttpRequest):
+    return render(request,"main/Form.html")
+def ccs_practice_view(request:HttpRequest):
+    return render(request,"main/LearnCSS.html")
+def css_introduction_view(request:HttpRequest):
+    return render(request,"main/FavouriteTeamWithCSS.html")
+def ai_revolution_view(request:HttpRequest):
+    return render(request,"main/AIRevolution.html")
+
+def careers_cv_view(request:HttpRequest):
+    return render(request,"main/cv.html")
