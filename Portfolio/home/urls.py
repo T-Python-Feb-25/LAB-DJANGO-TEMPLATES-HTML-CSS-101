31a83f151f5c607f9b6cfe6e0d054d03c2baa652
elaf <elafabdualrahman00@gmail.com>
2025-03-01 04:00:19 +0300

    created mini projects

---
+++ Portfolio/home/urls.py
@@ -0,0 +1,12 @@
+# home/urls.py
+from django.urls import path
+from . import views
+
+app_name = 'home'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('projects/', views.projects, name='projects'),
+    path('contact/', views.contact, name='contact'),
+]
---
+++ Portfolio/home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home/home.html')
+
+def about(request):
+    return render(request, 'home/about.html')
+
+def projects(request):
+    return render(request, 'home/projects.html')
+
+def contact(request):
+    return render(request, 'home/contact.html')
